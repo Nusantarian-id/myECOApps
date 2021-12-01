855bccc82c168dfbc1f0e895d7e4c6144ee540c3
Dikky Setiawan <setyawandicky88@gmail.com>
2021-12-01 22:53:01 +0700

    update ya

---
+++ app/src/main/java/id/myeco/myeco/MainActivity.java
@@ -0,0 +1,70 @@
+package id.myeco.myeco;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.annotation.TargetApi;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    protected WebView webView;
+
+    @SuppressLint("SetJavaScriptEnabled")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        webView = findViewById(R.id.webview);
+        webView.loadUrl(getString(R.string.whatsapp_web_url));
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().setBuiltInZoomControls(true);
+        webView.getSettings().setDomStorageEnabled(true);
+        webView.getSettings().setDisplayZoomControls(false);
+        webView.getSettings().setUserAgentString(getString(R.string.user_agent_string));
+        webView.setWebViewClient(new NavWebViewClient());
+    }
+
+    private class NavWebViewClient extends WebViewClient {
+
+        @SuppressWarnings("deprecation")
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            final Uri uri = Uri.parse(url);
+            return handleUri(uri);
+        }
+
+        @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+            final Uri uri = request.getUrl();
+            return handleUri(uri);
+        }
+
+        private boolean handleUri(final Uri uri) {
+            final String host = uri.getHost();
+            if (host.contains(getString(R.string.whatsapp_host))) {
+                return false;
+            }
+
+            Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+            if (intent.resolveActivity(getPackageManager()) != null) {
+                startActivity(intent);
+            }
+            return true;
+        }
+    }
+
+
+    @Override
+    public void onBackPressed() {
+        if (webView.canGoBack()) webView.goBack();
+        else super.onBackPressed();
+    }
+}
